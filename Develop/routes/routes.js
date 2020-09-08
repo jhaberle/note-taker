f677947730ed01afe73cb65ef13a46d3817f1de5
Jake Haberle <jake.haberle@gmail.com>
2020-09-07 22:51:27 -0600

    updated routes and removed const express (dupe)

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -1,5 +1,4 @@
-const express = require("express");
-const PORT = 4000;
+
 
 // my code ----------------------------------------------
 const express = require("express");
@@ -7,59 +6,57 @@ const PORT = 4000;
 const app = express();
 const fs = require("fs");
 const path = require("path");
-const note = [];
+const notes = [];
 
-const writeFileAsync = util.promisify(fs.writeFile);
+
+
 
 // --------------middleware------------------
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'))
 
 
 
-// html links---------------------------------------
+// -----------------api readfile for db-----------------
 
 module.exports = function(app) {
-
-    app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "index.html"));
-      });
-      
-      app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname, "notes.html"));
-      });
-}
-
-module.exports(app) {
     app.get("/api/notes", function(req, res) {
-        fs.readFile("./db/db.json", "utf8", function(data, error) {
+        fs.readFile("../db/db.json", "utf8", function(data, error) {
             if (error) {
                 return console.log(error);
             }
 
             const JSONobject = JSON.parse(data)
 
-            return res.json(JSONobject);
-        })
-    })
-}
-
-// checking for notes with id's ---------------------------------------------------
-
-// app.get("/api/notes/:notes", function(req, res) {
-//   var chosen = req.params.$noteList;
-
-//   console.log(chosen);
-
-//   for (var i = 0; i < characters.length; i++) {
-//     if (chosen === characters[i].routeName) {
-//       return res.json(characters[i]);
-//     }
-//   }
-
-//   return res.json(false);
-// });
+            return res.json(JSONobject)
+        });
+        console.log("read notes api");
+    });
 
 
+// ------------------api post to db-------------------------
 
-// _______________________-------------------------------------______________---
+    app.post("/api/notes", function(req, res) {
+        const note = req.body;
+        console.log("post successful")
+        notes.push(note);
+        const noteJSON = JSON.stringify(notes);
+            function writeFile(){
+                fs.writeFile(__dirname + '../db/db.json', json, 'utf8',function(err) {
+  
+                 if (err){
+                    return console.log(err);
+                }
+                console.log("File Appended")
+            });
+        }
+        writeFile()
+      
+        res.json(notes);
+    });
+};
+
+app.listen(PORT, function () {
+    console.log("listening on port" + PORT);
+});
