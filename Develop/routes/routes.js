ed9abdcd62dbfe7e7839ba809c096f044364bc96
Jake Haberle <jake.haberle@gmail.com>
2020-09-08 21:30:47 -0600

    re-copied index.js from original

--- Develop/routes/routes.js
+++ Develop/routes/routes.js
@@ -1,6 +1,6 @@
 
 
-// my code ----------------------------------------------
+// global variables ----------------------------------------------
 const express = require("express");
 const PORT = 4000;
 const app = express();
@@ -10,11 +10,29 @@ const notes = [];
 
 
 
-
 // --------------middleware------------------
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
+
+// html links----------------------------------
+
+
+module.exports = function(app) {
+
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname,  "../public/index.html"));
+      });
+      ​
+      app.get("/notes", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+      });
+
+};
+
+  ​
+  
+
 
 
 
@@ -22,7 +40,7 @@ app.use(express.static('public'))
 
 module.exports = function(app) {
     app.get("/api/notes", function(req, res) {
-        fs.readFile("../db/db.json", "utf8", function(data, error) {
+        fs.readFile("./db/db.json", "utf8", function(data, error) {
             if (error) {
                 return console.log(error);
             }
@@ -43,12 +61,12 @@ module.exports = function(app) {
         notes.push(note);
         const noteJSON = JSON.stringify(notes);
             function writeFile(){
-                fs.writeFile(__dirname + '../db/db.json', json, 'utf8',function(err) {
+                fs.writeFile(__dirname + '/../db/db.json', json, 'utf8',function(err) {
   
                  if (err){
                     return console.log(err);
                 }
-                console.log("File Appended")
+                console.log("appended file");
             });
         }
         writeFile()
@@ -56,6 +74,8 @@ module.exports = function(app) {
         res.json(notes);
     });
 };
+
+require("./public/assets/js/index")(app);
 
 app.listen(PORT, function () {
     console.log("listening on port" + PORT);
